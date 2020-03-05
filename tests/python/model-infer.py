7b3978b42e019f123e991a0dd87a76bddb7c9d2a
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-03-05 17:49:58 +0800

    update ssd_mobilenet_coco test

--- tests/python/model-infer.py
+++ tests/python/model-infer.py
@@ -5,14 +5,14 @@ from cvm import utils
 
 import os
 
-CVMContext.set_global(CPU)
-# CVMContext.set_global(GPU)
+# CVMContext.set_global(CPU)
+CVMContext.set_global(GPU)
 # CVMContext.set_global(FORMAL)
 
 # model_root = "/home/serving/tvm-cvm/data/jetson/"
-# model_root = "/tmp/ssd_512_mobilenet1.0_coco_tfm/"
+model_root = "/tmp/ssd_512_mobilenet1.0_coco_tfm/"
 # model_root = "/data/std_out/resnet50_v2"
-model_root = "/data/std_out/ssd"
+# model_root = "/data/std_out/ssd"
 json, params = load_model(os.path.join(model_root, "symbol"),
                          os.path.join(model_root, "params"))
 
