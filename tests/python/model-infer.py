0aeb522dba9af83d13d82f5de46aa9f361cdba61
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-03-23 16:43:15 +0800

    update model infer

--- tests/python/model-infer.py
+++ tests/python/model-infer.py
@@ -4,14 +4,15 @@ from cvm.dll import CVMAPIGetOutputLength, CVMAPIFreeModel
 from cvm import utils
 
 import os
+import time
 
 # CVMContext.set_global(CPU)
 CVMContext.set_global(GPU)
 # CVMContext.set_global(FORMAL)
 
 # model_root = "/home/serving/tvm-cvm/data/jetson/"
-#  model_root = "/data/std_out/ssd_512_mobilenet1.0_coco_tfm/"
-model_root = "/data/std_out/resnet50_v2"
+model_root = "/data/std_out/ssd_512_mobilenet1.0_coco_tfm/"
+#  model_root = "/data/std_out/resnet50_v2"
 # model_root = "/data/std_out/ssd"
 # model_root = "/data/std_out/resnet50_mxg/"
 json, params = utils.load_model(
@@ -24,7 +25,12 @@ print(CVMAPIGetOutputLength(net))
 data_path = os.path.join(model_root, "data.npy")
 data = utils.load_np_data(data_path)
 
-out = CVMAPIInference(net, data)
+iter_num = 10
+start = time.time()
+for i in range(iter_num):
+    out = CVMAPIInference(net, data)
+end = time.time()
+print ("Infer Time: ", (end - start) / iter_num)
 
 CVMAPIFreeModel(net)
 
