e25e19cb90dc3282208902b46f3dd52776e57445
huwwds <huwwds@gmail.com>
2020-04-24 15:14:14 +0800

    optimize broadcast

--- src/runtime/opencl/ops/fpga/broadcast.cpp
+++ src/runtime/opencl/ops/fpga/broadcast.cpp
@@ -1,3 +1,5 @@
+const int MAX_DIM = 6;
+
 int broadcast_eval(int type, int a, int b, int &c)
 {
     if (type == 0) //broadcast_add
@@ -14,7 +16,7 @@ int broadcast_eval(int type, int a, int b, int &c)
         c = a > b;
 }
 
-int broadcast_i_index(int *oshape, int o_index, int *ishape, int idim, int odim)
+int broadcast_i_index(int oshape[], int o_index, int ishape[], int idim, int odim)
 {
     if (idim == 1 && ishape[0] == 1)
         return 0;
@@ -34,59 +36,69 @@ int broadcast_i_index(int *oshape, int o_index, int *ishape, int idim, int odim)
     return index;
 }
 
-int getSize(int *shape, int ndim)
-{
-    int size = 1;
-    for (int i = 0; i < ndim; i++)
-    {
-        size *= shape[i];
-    }
-    return size;
-}
-
 extern "C"
 {
     void broadcast(
         int *a, int *b, int *c,
-        int *ashape, int adim,
-        int *bshape, int bdim,
-        int *cshape, int cdim,
+        int asize, int bsize, int csize,
+        int adim, int bdim, int cdim,
+        int ashape0, int ashape1, int ashape2, int ashape3, int ashape4, int ashape5,
+        int bshape0, int bshape1, int bshape2, int bshape3, int bshape4, int bshape5,
+        int cshape0, int cshape1, int cshape2, int cshape3, int cshape4, int cshape5,
         int type)
     {
 #pragma HLS INTERFACE m_axi port = a offset = slave bundle = gmem
 #pragma HLS INTERFACE m_axi port = b offset = slave bundle = gmem
 #pragma HLS INTERFACE m_axi port = c offset = slave bundle = gmem
 
-#pragma HLS INTERFACE m_axi port = ashape offset = slave bundle = gmem
-#pragma HLS INTERFACE m_axi port = bshape offset = slave bundle = gmem
-#pragma HLS INTERFACE m_axi port = cshape offset = slave bundle = gmem
-
 #pragma HLS INTERFACE s_axilite port = a bundle = control
 #pragma HLS INTERFACE s_axilite port = b bundle = control
 #pragma HLS INTERFACE s_axilite port = c bundle = control
-#pragma HLS INTERFACE s_axilite port = ashape bundle = control
-#pragma HLS INTERFACE s_axilite port = bshape bundle = control
-#pragma HLS INTERFACE s_axilite port = cshape bundle = control
 #pragma HLS INTERFACE s_axilite port = adim bundle = control
 #pragma HLS INTERFACE s_axilite port = bdim bundle = control
 #pragma HLS INTERFACE s_axilite port = cdim bundle = control
 #pragma HLS INTERFACE s_axilite port = type bundle = control
 
+#pragma HLS INTERFACE s_axilite port = asize bundle = control
+#pragma HLS INTERFACE s_axilite port = bsize bundle = control
+#pragma HLS INTERFACE s_axilite port = csize bundle = control
+
+#pragma HLS INTERFACE s_axilite port = ashape0 bundle = control
+#pragma HLS INTERFACE s_axilite port = ashape1 bundle = control
+#pragma HLS INTERFACE s_axilite port = ashape2 bundle = control
+#pragma HLS INTERFACE s_axilite port = ashape3 bundle = control
+#pragma HLS INTERFACE s_axilite port = ashape4 bundle = control
+#pragma HLS INTERFACE s_axilite port = ashape5 bundle = control
+
+#pragma HLS INTERFACE s_axilite port = bshape0 bundle = control
+#pragma HLS INTERFACE s_axilite port = bshape1 bundle = control
+#pragma HLS INTERFACE s_axilite port = bshape2 bundle = control
+#pragma HLS INTERFACE s_axilite port = bshape3 bundle = control
+#pragma HLS INTERFACE s_axilite port = bshape4 bundle = control
+#pragma HLS INTERFACE s_axilite port = bshape5 bundle = control
+
+#pragma HLS INTERFACE s_axilite port = cshape0 bundle = control
+#pragma HLS INTERFACE s_axilite port = cshape1 bundle = control
+#pragma HLS INTERFACE s_axilite port = cshape2 bundle = control
+#pragma HLS INTERFACE s_axilite port = cshape3 bundle = control
+#pragma HLS INTERFACE s_axilite port = cshape4 bundle = control
+#pragma HLS INTERFACE s_axilite port = cshape5 bundle = control
+
 #pragma HLS INTERFACE s_axilite port = return bundle = control
 
-        int a_size = getSize(ashape, adim);
-        int b_size = getSize(bshape, bdim);
-        int c_size = getSize(cshape, cdim);
+        int ashape[MAX_DIM] = {ashape0, ashape1, ashape2, ashape3, ashape4, ashape5};
+        int bshape[MAX_DIM] = {bshape0, bshape1, bshape2, bshape3, bshape4, bshape5};
+        int cshape[MAX_DIM] = {cshape0, cshape1, cshape2, cshape3, cshape4, cshape5};
 
-        if (a_size == 1)
-            for (int i = 0; i < c_size; i++)
+        if (asize == 1)
+            for (int i = 0; i < csize; i++)
                 broadcast_eval(type, a[0], b[i], c[i]);
-        else if (b_size == 1)
-            for (int i = 0; i < c_size; i++)
+        else if (bsize == 1)
+            for (int i = 0; i < csize; i++)
                 broadcast_eval(type, a[i], b[0], c[i]);
         else
         {
-            for (int i = 0; i < c_size; i++)
+            for (int i = 0; i < csize; i++)
             {
 #pragma HLS PIPELINE II = 1
                 int a_index = broadcast_i_index(cshape, i, ashape, adim, cdim);
