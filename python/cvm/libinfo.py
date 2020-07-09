8ac82ed04275b3493b7a759ef1cc21622e1c93fb
Longtao Wang <longtao.wang@cortexlabs.ai>
2020-07-09 18:25:12 +0800

    update python package install

--- python/cvm/libinfo.py
+++ python/cvm/libinfo.py
@@ -6,13 +6,12 @@ def find_lib_path():
     cvm_dir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
     source_dir = os.path.join(cvm_dir, "..", "..")
 
-    dll_path = []
-    dll_path.append(os.path.join(source_dir))
+    dll_path = [cvm_dir, source_dir]
     dll_path.append(os.path.join(source_dir, "build"))
 
     dll_path = [os.path.realpath(x) for x in dll_path]
 
-    lib_names = ["libcvm_runtime.so", "libcvm_runtime.dylib"]
+    lib_names = ["libcvm.so", "libcvm.dylib"]
     lib_dll_path = [os.path.join(p, ln) for ln in lib_names for p in dll_path]
 
     lib_found = [p for p in lib_dll_path if os.path.exists(p) and os.path.isfile(p)]
@@ -23,3 +22,5 @@ def find_lib_path():
                    str('\n'.join(lib_dll_path)))
         raise RuntimeError(message)
     return lib_found
+
+__VERSION__ = "0.1.0"
