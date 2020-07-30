c8cca523e930f8e1bc41e756f1d5502fe8bcb2fc
ryt <tang_1995@126.com>
2020-07-30 12:56:37 +0800

    update

--- python/mrt/tfm_ops.py
+++ python/mrt/tfm_ops.py
@@ -1560,67 +1560,6 @@ class BatchNorm(Transformer):
 
 @register_pass("validate")
 @register_pass("fuse_transpose")
-@register_transformer("InstanceNorm")
-class InstanceNorm(Transformer):
-    def rewrite(self, op, **kwargs):
-        params, infer_shapes = kwargs["params"], kwargs["infer_shapes"]
-        name = op.attr('name')
-        childs, attr = sym_iter(op.get_children()), op.list_attr()
-        X, X_name = childs[0], childs[0].attr('name')
-        gamma = params[childs[1].attr('name')]
-        beta = params[childs[2].attr('name')]
-        data_mean = params[childs[3].attr('name')]
-        data_var = params[childs[4].attr('name')]
-
-        fix_gamma = get_attr(attr, 'fix_gamma', True)
-        gamma = 1 if fix_gamma else gamma
-        # axis = get_attr(attr, 'axis', 1)
-
-        epsilon = float(attr['eps']) if 'eps' in attr else 1e-5
-        sc = gamma / nd.sqrt(data_var + epsilon)
-        bias = beta - sc * data_mean
-        if X.attr('op_name') == 'Convolution':
-            # Since convolution is "NCHW" format, axis must be one
-            assert axis == 1, "Channel in input must be axis 1"
-            cchilds, cattr = sym_iter(X.get_children()), X.list_attr()
-
-            conv_name = N.n(name)
-            W_name = cchilds[1].attr('name')
-            weight = params[W_name]
-            wn = N.n(W_name)
-            params[wn] = weight * sc.reshape(*sc.shape, 1, 1, 1)
-            W = mx.sym.var(wn, shape=params[W_name].shape)
-
-            B_name = N.n('bias')
-            if not get_attr(cattr, 'no_bias', False):
-                B_name = cchilds[2].attr('name')
-                bias += params[B_name]
-            params[B_name] = bias
-            B = mx.sym.var(B_name, shape=bias.shape)
-
-            cattr['no_bias'] = False
-            op = mx.sym.Convolution(cchilds[0], W,
-                                    B, **cattr, name=conv_name)
-        else:
-            ishp = infer_shapes[X_name][get_entry_id(X)]
-            reshp = [s if i == axis else 1 for i, s in enumerate(ishp)]
-            w_name = N.n('weight')
-            params[w_name] = sc.reshape(reshp)
-            W = mx.sym.var(w_name, shape=reshp)
-            node = mx.sym.broadcast_mul(X, W, name=N.n("broadcast_mul"))
-            bias_name = N.n('bias')
-            params[bias_name] = bias.reshape(reshp)
-            B = mx.sym.var(bias_name, shape=reshp)
-            op = mx.sym.broadcast_add(node, B, name=N.n("broadcast_add"))
-        return op
-
-    def calculate_ops(self, op, **kwargs):
-        kwargs['base_ops'] = 4
-        return super().calculate_ops(op, **kwargs)
-
-
-@register_pass("validate")
-@register_pass("fuse_transpose")
 @register_pass("rewrite")
 @register_pass("quantize")
 @register_pass("calculate_ops")
