7bb96606d95407cbf4d505a7e81343a99d558426
ryt <tang_1995@126.com>
2020-10-19 16:55:57 +0800

    update cvm_conv_2d_channel

--- python/mrt/gen/cvm_op.py
+++ python/mrt/gen/cvm_op.py
@@ -48,11 +48,13 @@ class RightShiftChannel(mx.operator.CustomOp):
 
 
 class Conv2DChannel(mx.operator.CustomOp):
-    def __init__(self, **kwargs):
+    def __init__(
+        self, dilate, kernel, layout, no_bias,
+        num_filter, num_group, pad, stride, **kwargs):
         pass
 
     def forward(self, is_train, req, in_data, out_data, aux):
-        pass
+        assert is_train == False
 
     def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
         assert False
@@ -84,4 +86,83 @@ class RightShiftChannelProp(mx.operator.CustomOpProp):
 
 @mx.operator.register("cvm_conv2d_channel")
 class Conv2DChannelProp(mx.operator.CustomOpProp):
-    pass
+    def __init__(
+        self, dilate, kernel, layout, num_filter, num_group, pad, stride):
+        self.dilate = dilate
+        self.kernel = kernel
+        self.layout = layout
+        self.num_filter = num_filter
+        self.num_group = num_group
+        self.pad = pad
+        self.stride = stride
+        super(Conv2DChannelProp, self).__init__(need_top_grad=True)
+    def list_arguments(self):
+        return ['data', 'weight']
+    def list_outputs(self, in_shape):
+        return ['output']
+    def infer_shape(self, in_shape):
+        # verify layout
+        assert self.layout == "NCHW", \
+            "cvm_conv2d_channel only supports layout: NCHW vs {}".format(
+            self.layout)
+        # verify shapes
+        assert len(in_shape) == 2, "invalid in_shape: {}".format(in_shape)
+        X_shape, W_shape = in_shape
+        assert len(X_shape) == 4, \
+            "input data should be 4d: {}".format(X_shape)
+        for attr, name in [
+            (self.kernel, "kernel"), (self.pad, "pad"),
+            (self.strides, "strides"), (self.dilate, "dilate"),
+        ]:
+            attr = eval(attr)
+            assert len(attr) == 2, \
+                "invalid attr, name: {}, value: {}".format((name, attr))
+            verify_attr_range(attr[0], name)
+            verify_attr_range(attr[1], name)
+        assert len(W_shape) == 4, \
+            "input weight should be 4d: {}".format(W_shape)
+        # verify num_group
+        num_group = eval(self.num_group)
+        assert num_group > 0 and \
+            X_shape[1] % num_group == 0 and W_shape[0] % num_group == 0, \
+            "invalid num_group: {}, X_shape: {}, W_shape: {}".format( \
+            (num_group, X_shape, W_shape))
+        # verify kernel shape
+        KH, KW = eval(self.kernel)
+        assert KH == W_shape[2] and KW == W_shape[3], \
+            "invalid kernel attr, kernel: {}, W_shape: {}".format( \
+            (self.kernel, W_shape))
+        DH, DW = eval(self.dilate)
+        DH_size = 1 + (KH-1)*DH
+        DW_size = 1 + (KW-1)*DW
+        PH, PW = eval(self.pad)
+        assert DH_size < H+2*PH and DW_size < W+2*PW, \
+            "invalid kernel attr, kernel: {}, pad: {}, dilate: {}".format( \
+            (self.kernel, self.pad, self.dilate))
+        # output shape
+        # TODO(archRev): out_shape[1] when num_group > 1
+        out_shape = [X_shape[0], W_shape[0], 0, 0]
+        SH, SW = eval(self.stride)
+        H, W = in_shape[2:]
+        if H != 0:
+            out_shape[2] = (H+PH*2-DH_size) // SH + 1
+        if W != 0:
+            out_shape[3] = (W+PW*2-DW_size) // SW + 1
+        out_shape = tuple(out_shape)
+        return [X_shape, W_shape], [out_shape], []
+    def infer_type(self, in_type):
+        X_type = in_type[0]
+        W_type = in_type[1]
+        return [X_type, B_type], [X_type], []
+    def create_operator(self, ctx, shapes, dtypes):
+        return Conv2DChannel(
+            self.dilate, self.kernel ,self.layout, self.num_filter,
+            self.num_group, self.pad, self.stride)
+
+ATTR_MIN_VALIE = 0
+ATTR_MAX_VALUE = 4096
+
+def verify_attr_range(val, name, minv=ATTR_MIN_VALUE, maxv=ATTR_MAX_VALUE):
+    assert minv <= val <= maxv, \
+        "val: {} not in valid range, name: {}, minv: {}, maxv: {}".format(
+        (val, name, minv, maxv))
