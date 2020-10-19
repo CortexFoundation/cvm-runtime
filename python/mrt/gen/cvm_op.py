b5c698ec585dc18b01c9ce113566db1524794106
ryt <tang_1995@126.com>
2020-10-19 14:44:52 +0800

    update cvm_right_shift_channel

--- python/mrt/gen/cvm_op.py
+++ python/mrt/gen/cvm_op.py
@@ -5,14 +5,25 @@ import numpy as np
 
 
 class RightShiftChannel(mx.operator.CustomOp):
-    def __init__(self, precs, sbs, **kwargs):
-        assert len(precs) == len(sbs)
-        self.nc = len(precs)
-        print(self.nc, "*****", precs)
-        clips = [2**(int(prec)-1)-1 for prec in precs.split(",")]
+    def __init__(self, precs, sbs, num_channel, inc, **kwargs):
+        self.inc = eval(inc)
+        self.num_channel = eval(num_channel)
+        precs = [eval(prec) for prec in precs.split(',')]
+        sbs = [eval(sb) for sb in sbs.split(',')]
+        assert len(precs) == len(sbs), \
+            "invalid length, length of precs: {}, length of sbs: {}".format( \
+            (precs, sbs))
+        assert self.num_channel % self.inc == 0, \
+            "the number of channels: {} must be divisible by inc: {}".format( \
+            (self.num_channel, self.inc))
+        assert self.num_channel == self.inc * len(precs), \
+            "the multiplication of the length of precs: {} and inc: {} " + \
+            "must be equal to the number of channels: {}".format( \
+            (len(precs), self.inc, self.num_channel))
+        clips = [2**(int(prec)-1)-1 for prec in precs]
         self.mins = [int(-clip) for clip in clips]
         self.maxs = [int(clip) for clip in clips]
-        self.sbs = [int(sb) for sb in sbs.split(",")]
+        self.sbs = [int(sb) for sb in sbs]
         assert all([sb > 0 for sb in self.sbs])
 
     def forward(self, is_train, req, in_data, out_data, aux):
@@ -20,14 +31,16 @@ class RightShiftChannel(mx.operator.CustomOp):
         X = in_data[0]
         amins, amaxs = self.mins, self.maxs
         out = X.round()
-        for i in range(self.nc):
-            if self.sbs[i] > 1:
-                out[:,i] = out[:,i] / (2 ** (self.sbs[i]-1))
-                out[:,i] = out[:,i].floor()
-            out[:,i] = out[:,i] + 1
-            out[:,i] = out[:,i] / 2
-            out[:,i] = out[:,i].floor()
-            out[:,i] = out[:,i].clip(a_min=amins[i], a_max=amaxs[i])
+        for i in range(0, self.num_channel, self.inc):
+            j = i+self.inc
+            k = i // self.inc
+            if self.sbs[k] > 1:
+                out[:,i:j] = out[:,i:j] / (2 ** (self.sbs[k]-1))
+                out[:,i:j] = out[:,i:j].floor()
+            out[:,i:j] = out[:,i:j] + 1
+            out[:,i:j] = out[:,i:j] / 2
+            out[:,i:j] = out[:,i:j].floor()
+            out[:,i:j] = out[:,i:j].clip(a_min=amins[k], a_max=amaxs[k])
         self.assign(out_data[0], req[0], out)
 
     def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
@@ -47,9 +60,11 @@ class Conv2DChannel(mx.operator.CustomOp):
 
 @mx.operator.register("cvm_right_shift_channel")
 class RightShiftChannelProp(mx.operator.CustomOpProp):
-    def __init__(self, precs, sbs):
+    def __init__(self, precs, sbs, num_channel, inc):
         self.precs = precs
         self.sbs = sbs
+        self.num_channel = num_channel
+        self.inc = inc
         super(RightShiftChannelProp, self).__init__(need_top_grad=True)
     def list_arguments(self):
         return ['data']
@@ -63,7 +78,8 @@ class RightShiftChannelProp(mx.operator.CustomOpProp):
         X_type = in_type[0]
         return [X_type], [X_type], []
     def create_operator(self, ctx, shapes, dtypes):
-        return RightShiftChannel(self.precs, self.sbs)
+        return RightShiftChannel(
+            self.precs, self.sbs, self.num_channel, self.inc)
 
 
 @mx.operator.register("cvm_conv2d_channel")
--- tests/mrt/gen/cvm_right_shift_channel.py
+++ tests/mrt/gen/cvm_right_shift_channel.py
@@ -2,25 +2,81 @@ import mxnet as mx
 import numpy as np
 from mxnet import ndarray as nd
 from mrt.gen import cvm_op
+from mrt import cvm_op
 
-def test_symbol():
+def test_symbol(precs, sbs, num_channel, inc):
     X = mx.sym.var("var")
-    precs = "8,8,8"
-    sbs = "8,8,8"
     op = mx.sym.Custom(
-        X, precs=precs, sbs=sbs, name="out",
+        X, precs=precs, sbs=sbs, num_channel=num_channel, inc=inc, name="out",
         op_type="cvm_right_shift_channel")
 
-def test_nd():
-    X = np.random.rand(16, 3, 224, 224)
-    X = nd.array(X)
-    precs = "8,8,8"
-    sbs = "8,8,8"
+def check_valid(A, B, tol=1e-6):
+    assert A.shape == B.shape, \
+        "invalid shape, shape of A: {}, shape of B: {}".format(
+        (A.shape, B.shape))
+    Xs = []
+    for X in [A, B]:
+        assert isinstance(X, nd.NDArray), \
+            "invalid type, type of X: %s".format(type(X))
+        X = X.asnumpy()
+        X = np.ndarray.flatten(X)
+        X = X.tolist()
+        assert isinstance(X, list), "invalid type, type of X: {}".format(type(X))
+        Xs.append(X)
+    A, B = Xs
+    assert len(A) == len(B), \
+        "invalid length, length of A: {}, length of B: {}".format(
+        (len(A), len(B)))
+    for i in range(len(A)):
+        diff = abs(A[i] - B[i])
+        assert diff <= tol, "invalid tolerance, tol: {}, diff: {}".format(
+        (tol, diff))
+    print("check valid")
+
+def forward_right_shift_channel(X, precs, sbs, inc):
+    num_channel = X.shape[1]
     out = nd.Custom(
-        X, precs=precs, sbs=sbs,
+        X, precs=precs, sbs=sbs, num_channel=num_channel, inc=inc,
         op_type="cvm_right_shift_channel")
-    print(type(out))
+    return out
+
+def forward_slice_right_shift(X, precs, sbs, inc):
+    precs = [eval(prec) for prec in precs.split(',')]
+    sbs = [eval(sb) for sb in sbs.split(',')]
+    xshp = X.shape
+    ndim = len(xshp)
+    assert xshp[1] % len(precs) == 0, \
+        "division error, xshp[0]: {}, len(precs): {}".format( \
+        (xshp[1], len(precs)))
+    assert inc == xshp[1] // len(precs), \
+        "invalid inc, inc: {}, xshp[1]//len(precs): {}".format( \
+        (inc, xshp[1]//len(precs)))
+    nodes = []
+    for i in range(0, xshp[1], inc):
+        begin = (None,i,) + (None,)*(ndim-2)
+        end = (None,i+inc) + (None,)*(ndim-2)
+        node = nd.slice(X, begin=begin, end=end)
+        j = i // inc
+        node = nd.Custom(
+            node, precision=precs[j], shift_bit=sbs[j],
+            op_type="cvm_right_shift")
+        nodes.append(node)
+    out = nd.concat(*nodes, dim=1)
+    return out
+
+def generate_data_int(shp, th=2**24):
+    X = np.random.random(shp)*th*2 - th
+    X = X.round()
+    X = nd.array(X)
+    return X
+
+def test_nd(shp, precs, sbs, inc, tol=1e-6):
+    X = generate_data_int(shp)
+    out1 = forward_right_shift_channel(X, precs=precs, sbs=sbs, inc=inc)
+    out2 = forward_slice_right_shift(X, precs=precs, sbs=sbs, inc=inc)
+    check_valid(out1, out2, tol=tol)
 
 if __name__ == "__main__":
-    test_symbol()
-    test_nd()
+    test_symbol(precs="8,8,8", sbs="8,8,8", num_channel=3, inc=1)
+    test_nd(shp=(16,3,224,224), precs="8,8,8", sbs="8,8,8", inc=1)
+    test_nd(shp=(16,128,32,32), precs="8,7,8,8", sbs="8,8,8,8", inc=32)
