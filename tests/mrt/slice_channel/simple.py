update slicechannel: fix bug for precision

tests/mrt/slice_channel/simple.py
@@ -4,7 +4,7 @@ import slice_utils as utils
 from mxnet import ndarray as nd
 
 if __name__ == '__main__':
-    a = nd.array(np.random.random((1,32,28,28)))
+    a = nd.array(np.random.random((16,32,28,28)))
     # a = nd.array([-1,2]).reshape((1,2,1,1))
     # print("x", a)
     b = nd.array(np.random.random((4,32,1,1)))
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     b1 = b1.reshape((128,1,1,1))
     c1 = nd.Convolution(
         a, b1, no_bias=True, num_group=32, kernel=(1,1), num_filter=128)
-    c1 = c1.reshape((1,32,4,28,28))
+    c1 = c1.reshape((16,32,4,28,28))
     # print("gconv", c1)
     c1 = nd.sum(c1, axis=1)
     # print("sum", c1)

tests/mrt/slice_channel/slice_utils.py
@@ -24,14 +24,15 @@ def check_valid(A, B, tol=1e-6):
             "invalid tolerance, tol: {}, diff: {}".format(tol, diff)
     print("check valid")
 
-def generate_data_int(shp, th=2**24):
+def generate_data_int(shp, th=2**8):
     X = np.random.random(shp)*th*2 - th
     X = X.round()
     X = nd.array(X)
     return X
 
-def test_nd(shps, attrs, fwd_func, cmp_func, tol=1e-6):
-    X = [generate_data_int(shp) for shp in shps]
+def test_nd(
+    shps, attrs, fwd_func, cmp_func, tol=1e-6, th=2**8):
+    X = [generate_data_int(shp, th=th) for shp in shps]
     out1 = fwd_func(*X, **attrs)
     out2 = cmp_func(*X, **attrs)
     check_valid(out1, out2, tol=tol)