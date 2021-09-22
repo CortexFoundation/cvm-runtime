365f5db01a6d40b996ed61b79c54994fa6029121
ryt <tang_1995@126.com>
2021-09-22 16:39:47 +0800

    upt

--- python/mrt/mrt_entry.py
+++ python/mrt/mrt_entry.py
@@ -7,8 +7,10 @@ import mxnet as mx
 from mrt.gluon_zoo import save_model
 from mrt.common import log
 from mrt import utils
-from mrt.transformer import Model
+from mrt.transformer import Model, MRT
 from mrt import dataset as ds
+from mrt import sym_utils as sutils
+from mrt import sim_quant_helper as sim
 
 default_batch = 16
 default_ctx = mx.cpu()
@@ -169,5 +171,117 @@ def mrt_calibrate(
     save_conf(model_prefix+".calibrate.conf", logger=logger, **conf_map)
     logger.info("calibrate stage finished")
 
-def mrt_quantize():
-    pass
+def mrt_quantize(
+    model_dir, model_name, verbosity, restore_names, input_precision,
+    output_precision, device_type, device_ids, softmax_lambd, shift_bits,
+    thresholds, attribute_deps, oscale_maps):
+    model_prefix = get_model_prefix(model_dir, model_name)
+    logger = get_logger(verbosity)
+    conf_calib_file = model_prefix + ".calibrate.conf"
+    check_file_existance(conf_calib_file, logger=logger)
+    conf_map = load_conf(conf_calib_file, logger=logger)
+    sym_calib_file, prm_calib_file, ext_calib_file = load_fname(
+        model_prefix, suffix="mrt.calibrate", with_ext=True)
+    check_file_existance(
+        sym_calib_file, prm_calib_file, ext_calib_file, logger=logger)
+    mrt = MRT.load(model_name+".mrt.calibrate", datadir=model_dir)
+    conf_quant_file = model_prefix + ".quantize.conf"
+
+    # restoration configuration
+    name_to_op = {}
+    for sym in sutils.topo_sort(mrt.current_model.symbol):
+        name, op_name = sym.attr('name'), sym.attr('op_name')
+        if op_name not in name_to_op:
+            name_to_op[op_name] = []
+        name_to_op[op_name].append(name)
+    new_names = []
+    for name in restore_names:
+        if name.startswith("_OP_") and name[4:] in name_to_op:
+            for new_name in name_to_op[name[4:]]:
+                new_names.append(new_name)
+        else:
+            new_names.append(name)
+    restore_names = set(new_names)
+    if '_ALL_EXCEPT_' in restore_names:
+        from tfm_base import _pass_manager
+        from tfm_ops import disabled_restore_ops
+
+        quantize_ops = [op_name for op_name in _pass_manager["quantize"] \
+                        if op_name not in disabled_restore_ops]
+        restore_names_new = []
+        for sym in sutils.topo_sort(mrt.current_model.symbol):
+            name, op_name = sym.attr('name'), sym.attr('op_name')
+            if op_name in quantize_ops and \
+                name not in restore_names:
+                restore_names_new.append(name)
+        restore_names = set(restore_names_new)
+    for name in restore_names:
+        mrt.set_restore(name)
+
+    # hyper parameters configuration
+    if input_precision is not None:
+        mrt.set_input_prec(input_precision)
+    if output_precision is not None:
+        mrt.set_output_prec(output_precision)
+    ctx = get_ctx(device_type, device_ids)
+    if softmax_lambd is not None:
+        mrt.set_softmax_lambd(softmax_lambd)
+    if shift_bits is not None:
+        mrt.set_shift_bits(shift_bits)
+    if thresholds is not None:
+        thresholds = json.loads(thresholds)
+        for name, threshold in thresholds.items():
+            mrt.set_threshold(name, threshold)
+
+    # quantization
+    mrt.quantize()
+    mrt.save(model_name+".mrt.quantize", datadir=model_dir)
+    input_shape = conf_map["input_shape"]
+    oscales = mrt.get_output_scales()
+    inputs_ext = mrt.get_inputs_ext()
+    infos = [oscales, inputs_ext]
+    ext_all_file = model_prefix + ".all.quantize.ext"
+    sim.save_ext(ext_all_file, *infos)
+    save_conf(conf_quant_file, logger=logger, **conf_map)
+    logger.info("quantization stage finished")
+
+    # mergemodel
+    if conf_map.get("split_keys", "") != "":
+        qmodel = mrt.current_model
+        if attribute_deps is None:
+            raise RuntimeError("model merging, please specify --attribute_deps")
+        attribute_deps = json.loads(attribute_deps)
+        mrt_oscales = mrt.get_output_scales()
+        name_idx = {mrt.get_maps().get(
+            s.attr("name"), s.attr("name")): i \
+            for i, s in enumerate(qmodel.symbol)}
+        def mergefunc(node, params, graph):
+            name, op_name = node.attr("name"), node.attr("op_name")
+            childs, attr = sutils.sym_iter(
+                node.get_children()), node.list_attr()
+            if op_name in attribute_deps:
+                attr_deps = attribute_deps[op_name]
+                for attr_name, v in attr_deps.items():
+                    val = sutils.get_attr(attr, attr_name, 0)
+                    attr[attr_name] = int(val*mrt_oscales[name_idx[v]])
+                node = sutils.get_mxnet_op(op_name)(
+                    *childs, **attr, name=name)
+            return node
+        sym_top_file, prm_top_file = load_fname(model_prefix, suffix="top")
+        check_file_existance(sym_top_file, prm_top_file, logger=logger)
+        top = Model.load(sym_top_file, prm_top_file)
+        model_merger = Model.merger(qmodel, top, mrt.get_maps())
+        qmodel = model_merger.merge(callback=mergefunc)
+        if oscale_maps is None:
+            raise RuntimeError("model merging, please specify --oscale_maps")
+        oscale_maps = json.loads(oscale_maps)
+        oscales = model_merger.get_output_scales(mrt_oscales, oscale_maps)
+        sym_all_file, prm_all_file, ext_all_file = load_fname(
+            model_prefix, suffix="all.quantize", with_ext=True)
+        qmodel.save(sym_all_file, prm_all_file)
+        infos = [oscales, inputs_ext]
+        sim.save_ext(ext_all_file, *infos)
+        save_conf(conf_quant_file, logger=logger, **conf_map)
+        logger.info("model merging finished")
+    else:
+        logger.info("model merging skipped")
