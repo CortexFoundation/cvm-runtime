8264e4e056a874eba93b25f29df87c937cf4abd7
ryt <tang_1995@126.com>
2021-10-23 11:35:41 +0800

    upt

--- python/mrt/frontend/polls/views.py
+++ python/mrt/frontend/polls/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the ppolls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def redirect_to_year(request):
+    year = 2006
+    return HttpResponseRedirect(reverse('news-year-archieve'), args=(year,))
