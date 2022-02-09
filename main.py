5e49d503a9e571391a097f67b6059412b3208cc0
ryt <tang_1995@126.com>
2022-02-09 16:51:09 +0800

    [prune] remove redundancy for main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, "./python")
 
-from mrt.V3.utils import get_cfg_defaults, merge_cfg, override_cfg_args
+from mrt.V3.utils import merge_cfg, override_cfg_args
 from mrt.V3.execute import run
 from mrt.V3.utils import DOC as utils_doc
 from mrt.V3.prepare import DOC as prepare_doc
@@ -31,7 +31,6 @@ if __name__ == "__main__":
             "invalid length: {} of sys.argv: {}".format(
             len(sys.argv), sys.argv)
         yaml_file = sys.argv[1]
-        cfg = get_cfg_defaults()
         cfg = merge_cfg(yaml_file)
         cfg = override_cfg_args(cfg, sys.argv[2:])
         run(cfg)
